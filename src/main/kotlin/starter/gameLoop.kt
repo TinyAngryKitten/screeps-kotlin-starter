3876facc6fe47f675154bfd73b58983cdd8d8819
Sander Høyvik <sander.hoyvik@gmail.com>
2020-12-25 15:51:27 +0100

    put release files in release folder

--- src/main/kotlin/starter/gameLoop.kt
+++ src/main/kotlin/starter/gameLoop.kt
@@ -18,20 +18,24 @@ import screeps.utils.unsafe.delete
 import screeps.utils.unsafe.jsObject
 
 fun gameLoop() {
-    val mainSpawn: StructureSpawn = Game.spawns.values.firstOrNull() ?: return
+    console.log("look starting")
+    val mainSpawn: StructureSpawn = Game.spawns.values.firstOrNull() ?: return console.log("No spawn found, exiting...")
 
     //delete memories of creeps that have passed away
     houseKeeping(Game.creeps)
 
     // just an example of how to use room memory
     mainSpawn.room.memory.numberOfCreeps = mainSpawn.room.find(FIND_CREEPS).count()
+    console.log("spawning...")
 
     //make sure we have at least some creeps
     spawnCreeps(Game.creeps.values, mainSpawn)
 
+    console.log("building...")
     val controller = mainSpawn.room.controller
     controller?.let(::buildExtensionsCloseToController)
 
+    console.log("performing role actions...")
     for ((_, creep) in Game.creeps) {
         when (creep.memory.role) {
             Role.HARVESTER -> creep.harvest()
