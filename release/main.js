1444172a0f82036965b0a91a86662f0bb30779bd
builderbot <runner@fv-az60-573.ylksyweohfkenmvi1sndel3fmb.cx.internal.cloudapp.net>
2020-12-31 12:23:12 +0000

    add new release files

--- release/main.js
+++ release/main.js
@@ -2,6 +2,12 @@
   'use strict';
   var $$importsForInline$$ = _.$$importsForInline$$ || (_.$$importsForInline$$ = {});
   var equals = Kotlin.equals;
+  var Enum = Kotlin.kotlin.Enum;
+  var Kind_CLASS = Kotlin.Kind.CLASS;
+  var throwISE = Kotlin.throwISE;
+  var first = Kotlin.kotlin.collections.first_us0mfu$;
+  var getCallableRef = Kotlin.getCallableRef;
+  var isBlank = Kotlin.kotlin.text.isBlank_gw00vp$;
   var compareTo = $module$screeps_kotlin_types_jsLegacy.screeps.api.compareTo_4yg84r$;
   var asSequence = Kotlin.kotlin.collections.asSequence_us0mfu$;
   var filter = Kotlin.kotlin.sequences.filter_euau3h$;
@@ -9,14 +15,10 @@
   var firstOrNull = Kotlin.kotlin.sequences.firstOrNull_veqyi0$;
   var Unit = Kotlin.kotlin.Unit;
   var any = Kotlin.kotlin.sequences.any_veqyi0$;
-  var getCallableRef = Kotlin.getCallableRef;
   var ensureNotNull = Kotlin.ensureNotNull;
-  var Enum = Kotlin.kotlin.Enum;
-  var Kind_CLASS = Kotlin.Kind.CLASS;
-  var throwISE = Kotlin.throwISE;
   var listOf = Kotlin.kotlin.collections.listOf_i5x0yv$;
+  var IntRange = Kotlin.kotlin.ranges.IntRange;
   var until = Kotlin.kotlin.ranges.until_dqglrj$;
-  var toString = Kotlin.toString;
   var jsObject = $module$screeps_kotlin_types_jsLegacy.screeps.utils.unsafe.jsObject_7qq44f$;
   var options = $module$screeps_kotlin_types_jsLegacy.screeps.api.options_v9cmv8$;
   var ArrayList_init = Kotlin.kotlin.collections.ArrayList_init_ww73n8$;
@@ -26,19 +28,82 @@
   var Kind_INTERFACE = Kotlin.Kind.INTERFACE;
   var joinToString = Kotlin.kotlin.collections.joinToString_fmv235$;
   var split = Kotlin.kotlin.text.split_ip8yn$;
+  var firstOrNull_0 = Kotlin.kotlin.collections.firstOrNull_2p1efm$;
   var toInt = Kotlin.kotlin.text.toInt_pdl1vz$;
   var MemoryMappingDelegate = $module$screeps_kotlin_types_jsLegacy.screeps.utils.memory.MemoryMappingDelegate;
+  var emptyList = Kotlin.kotlin.collections.emptyList_287e2$;
   var memory = $module$screeps_kotlin_types_jsLegacy.screeps.utils.memory.memory_9ce4rd$;
   var PropertyMetadata = Kotlin.PropertyMetadata;
-  var emptyList = Kotlin.kotlin.collections.emptyList_287e2$;
   var getPropertyCallableRef = Kotlin.getPropertyCallableRef;
-  var firstOrNull_0 = Kotlin.kotlin.collections.firstOrNull_us0mfu$;
+  var firstOrNull_1 = Kotlin.kotlin.collections.firstOrNull_us0mfu$;
   var IllegalArgumentException_init = Kotlin.kotlin.IllegalArgumentException_init_pdl1vj$;
+  StructurePriority.prototype = Object.create(Enum.prototype);
+  StructurePriority.prototype.constructor = StructurePriority;
   Role.prototype = Object.create(Enum.prototype);
   Role.prototype.constructor = Role;
   function loop() {
     gameLoop();
   }
+  function get_priority($receiver) {
+    if (equals($receiver, STRUCTURE_SPAWN))
+      return StructurePriority$HIGH_getInstance();
+    else if (equals($receiver, STRUCTURE_TOWER))
+      return StructurePriority$HIGH_getInstance();
+    else if (equals($receiver, STRUCTURE_CONTAINER))
+      return StructurePriority$MEDIUM_getInstance();
+    else if (equals($receiver, STRUCTURE_EXTENSION))
+      return StructurePriority$MEDIUM_getInstance();
+    else
+      return StructurePriority$LOW_getInstance();
+  }
+  function StructurePriority(name, ordinal) {
+    Enum.call(this);
+    this.name$ = name;
+    this.ordinal$ = ordinal;
+  }
+  function StructurePriority_initFields() {
+    StructurePriority_initFields = function () {
+    };
+    StructurePriority$LOW_instance = new StructurePriority('LOW', 0);
+    StructurePriority$MEDIUM_instance = new StructurePriority('MEDIUM', 1);
+    StructurePriority$HIGH_instance = new StructurePriority('HIGH', 2);
+  }
+  var StructurePriority$LOW_instance;
+  function StructurePriority$LOW_getInstance() {
+    StructurePriority_initFields();
+    return StructurePriority$LOW_instance;
+  }
+  var StructurePriority$MEDIUM_instance;
+  function StructurePriority$MEDIUM_getInstance() {
+    StructurePriority_initFields();
+    return StructurePriority$MEDIUM_instance;
+  }
+  var StructurePriority$HIGH_instance;
+  function StructurePriority$HIGH_getInstance() {
+    StructurePriority_initFields();
+    return StructurePriority$HIGH_instance;
+  }
+  StructurePriority.$metadata$ = {
+    kind: Kind_CLASS,
+    simpleName: 'StructurePriority',
+    interfaces: [Enum]
+  };
+  function StructurePriority$values() {
+    return [StructurePriority$LOW_getInstance(), StructurePriority$MEDIUM_getInstance(), StructurePriority$HIGH_getInstance()];
+  }
+  StructurePriority.values = StructurePriority$values;
+  function StructurePriority$valueOf(name) {
+    switch (name) {
+      case 'LOW':
+        return StructurePriority$LOW_getInstance();
+      case 'MEDIUM':
+        return StructurePriority$MEDIUM_getInstance();
+      case 'HIGH':
+        return StructurePriority$HIGH_getInstance();
+      default:throwISE('No enum constant building.StructurePriority.' + name);
+    }
+  }
+  StructurePriority.valueOf_61zpoe$ = StructurePriority$valueOf;
   function buildExtensionsCloseToController(controller) {
     if (controller.level >= 2) {
       var $receiver = controller.room.find(FIND_MY_STRUCTURES);
@@ -73,8 +138,6 @@
           break;
       }
     }}
-  function buildRoadsToSources() {
-  }
   function buildRoad(from, to, room) {
     var $receiver = from.findPathTo(to);
     var tmp$;
@@ -111,30 +174,35 @@
       }
     }
   }
-  function build($receiver, assignedRoom) {
+  function build($receiver, assignedRoom, constructionSites) {
     if (assignedRoom === void 0)
       assignedRoom = $receiver.room;
+    if (constructionSites === void 0)
+      constructionSites = assignedRoom.find(FIND_MY_CONSTRUCTION_SITES);
+    var tmp$, tmp$_0, tmp$_1;
     if (!buildingIsPossible($receiver, assignedRoom))
       return doTemporaryTask($receiver);
     set_temporaryTask($receiver.memory, Role$UNASSIGNED_getInstance());
-    var tmp$ = get_building($receiver.memory);
-    if (tmp$) {
-      tmp$ = $receiver.store[RESOURCE_ENERGY] === 0;
-    }if (tmp$) {
+    var tmp$_2 = get_building($receiver.memory);
+    if (tmp$_2) {
+      tmp$_2 = $receiver.store[RESOURCE_ENERGY] === 0;
+    }if (tmp$_2)
       set_building($receiver.memory, false);
-      $receiver.say('\uD83D\uDD04 harvest');
-    }var tmp$_0 = !get_building($receiver.memory);
-    if (tmp$_0) {
-      tmp$_0 = $receiver.store[RESOURCE_ENERGY] == $receiver.store.getCapacity();
-    }if (tmp$_0) {
+    var tmp$_3 = !get_building($receiver.memory);
+    if (tmp$_3) {
+      tmp$_3 = $receiver.store[RESOURCE_ENERGY] == $receiver.store.getCapacity();
+    }if (tmp$_3)
       set_building($receiver.memory, true);
-      $receiver.say('\uD83D\uDEA7 build');
-    }if (get_building($receiver.memory)) {
-      var targets = assignedRoom.find(FIND_MY_CONSTRUCTION_SITES);
-      if (!(targets.length === 0)) {
-        if (equals($receiver.build(targets[get_resourceIndex($receiver.memory)]), ERR_NOT_IN_RANGE)) {
-          $receiver.moveTo(targets[get_resourceIndex($receiver.memory)].pos);
-        }}} else {
+    if (get_building($receiver.memory)) {
+      if (!siteIdIsValid(get_targetId($receiver.memory)))
+        set_targetId($receiver.memory, (tmp$_0 = (tmp$ = findHighestPriorityConstructionSite($receiver, constructionSites)) != null ? tmp$.id : null) != null ? tmp$_0 : '');
+      tmp$_1 = Game.getObjectById(get_targetId($receiver.memory));
+      if (tmp$_1 == null) {
+        return doTemporaryTask($receiver);
+      }var target = tmp$_1;
+      if (equals($receiver.build(target), ERR_NOT_IN_RANGE)) {
+        $receiver.moveTo(target.pos);
+      }} else {
       var sources = $receiver.room.find(FIND_SOURCES);
       if (equals($receiver.harvest(sources[0]), ERR_NOT_IN_RANGE)) {
         $receiver.moveTo(sources[0].pos);
@@ -142,6 +210,39 @@
   }
   function buildingIsPossible($receiver, room) {
     return !(room.find(FIND_CONSTRUCTION_SITES).length === 0);
+  }
+  function findHighestPriorityConstructionSite($receiver, list) {
+    var tmp$;
+    var tmp$_0;
+    if ((tmp$ = !(list.length === 0) ? list : null) != null) {
+      var initial = first(list);
+      var operation = getCallableRef('takeBestFittingConstructionSite', function ($receiver, p1, p2) {
+        return takeBestFittingConstructionSite($receiver, p1, p2);
+      }.bind(null, $receiver));
+      var tmp$_1;
+      var accumulator = initial;
+      for (tmp$_1 = 0; tmp$_1 !== tmp$.length; ++tmp$_1) {
+        var element = tmp$[tmp$_1];
+        accumulator = operation(accumulator, element);
+      }
+      tmp$_0 = accumulator;
+    } else
+      tmp$_0 = null;
+    return tmp$_0;
+  }
+  function takeBestFittingConstructionSite($receiver, site1, site2) {
+    if (get_priority(site1.structureType).compareTo_11rb$(get_priority(site2.structureType)) > 0)
+      return site1;
+    else if (get_priority(site1.structureType).compareTo_11rb$(get_priority(site2.structureType)) < 0)
+      return site2;
+    else
+      return takeClosest($receiver, site1, site2);
+  }
+  function takeClosest($receiver, site1, site2) {
+    return site2.pos.findPathTo($receiver.pos).length > site2.pos.findPathTo($receiver.pos).length ? site2 : site1;
+  }
+  function siteIdIsValid(id) {
+    return !isBlank(id) && Game.getObjectById(id) != null;
   }
   function harvestIsPossible($receiver, storeOwner) {
     return storeOwner != null && compareTo(storeOwner.store.getFreeCapacity(RESOURCE_ENERGY), 0) > 0;
@@ -171,28 +272,31 @@
       fromRoom = $receiver.room;
     if (toRoom === void 0)
       toRoom = $receiver.room;
-    console.log('harvesting...');
     var target = closestEnergyStoreWithSpace($receiver, toRoom);
     if (target == null || !harvestIsPossible($receiver, target))
       return doTemporaryTask($receiver);
-    console.log('should be possible...');
     set_temporaryTask($receiver.memory, Role$UNASSIGNED_getInstance());
     if (compareTo($receiver.store[RESOURCE_ENERGY], $receiver.store.getCapacity()) < 0) {
-      console.log('should harvest...');
       var sources = fromRoom.find(FIND_SOURCES);
-      if (equals($receiver.harvest(sources[get_resourceIndex($receiver.memory)]), ERR_NOT_IN_RANGE)) {
-        var error = $receiver.moveTo(sources[get_resourceIndex($receiver.memory)].pos);
-        if (equals(error, OK))
-          console.log('moving to resource...');
-        else
-          console.log($receiver.name + ' is unable to move due to: ' + error);
-      }} else {
+      goHarvest($receiver, sources[get_resourceIndex($receiver.memory)]);
+    } else {
       if (equals($receiver.transfer(target, RESOURCE_ENERGY), ERR_NOT_IN_RANGE)) {
         console.log('storing...');
-        var error_0 = $receiver.moveTo(target.pos);
-        if (!equals(error_0, OK))
-          console.log($receiver.name + ' is unable to move due to: ' + error_0);
+        var error = $receiver.moveTo(target.pos);
+        if (!equals(error, OK))
+          console.log($receiver.name + ' is unable to move due to: ' + error);
       }}
+  }
+  function goHarvest($receiver, source) {
+    var resultCode = $receiver.harvest(source);
+    if (equals(resultCode, ERR_NOT_IN_RANGE))
+      return walkTo($receiver, source.pos);
+    else
+      return resultCode;
+  }
+  function walkTo($receiver, targetPos) {
+    var result = $receiver.moveTo(targetPos);
+    return result;
   }
   function repair$lambda(this$repair) {
     return function () {
@@ -243,8 +347,7 @@
       else
         tmp$ = Role$UPGRADER_getInstance();
       set_temporaryTask(tmp$_0, tmp$);
-    }$receiver.say('temp: ' + get_temporaryTask($receiver.memory));
-    tmp$_1 = get_temporaryTask($receiver.memory);
+    }tmp$_1 = get_temporaryTask($receiver.memory);
     if (equals(tmp$_1, Role$HARVESTER_getInstance()))
       harvest($receiver);
     else if (equals(tmp$_1, Role$BUILDER_getInstance()))
@@ -277,11 +380,9 @@
   function upgrade_0($receiver, controller) {
     if ($receiver.store[RESOURCE_ENERGY] === 0 && get_building($receiver.memory)) {
       set_building($receiver.memory, false);
-      $receiver.say('collecting energy');
     } else {
       if ($receiver.store[RESOURCE_ENERGY] == $receiver.store.getCapacity() && !get_building($receiver.memory)) {
         set_building($receiver.memory, true);
-        $receiver.say('returning with energy');
       }}
     if (!get_building($receiver.memory)) {
       var sources = $receiver.room.find(FIND_SOURCES);
@@ -416,13 +517,13 @@
     var tmp$;
     var nrOfParts = (spawn.room.energyCapacityAvailable / 76 | 0) / 3 | 0;
     var $receiver = ArrayList_init_0();
-    var $receiver_0 = until(0, nrOfParts);
+    var $receiver_0 = new IntRange(0, nrOfParts);
     var destination = ArrayList_init(collectionSizeOrDefault($receiver_0, 10));
     var tmp$_0;
     tmp$_0 = $receiver_0.iterator();
     while (tmp$_0.hasNext()) {
       var item = tmp$_0.next();
-      destination.add_11rb$(WORK);
+      destination.add_11rb$(CARRY);
     }
     $receiver.addAll_brywnq$(destination);
     var $receiver_1 = until(0, nrOfParts);
@@ -431,7 +532,7 @@
     tmp$_1 = $receiver_1.iterator();
     while (tmp$_1.hasNext()) {
       var item_0 = tmp$_1.next();
-      destination_0.add_11rb$(CARRY);
+      destination_0.add_11rb$(WORK);
     }
     $receiver.addAll_brywnq$(destination_0);
     var $receiver_2 = until(0, nrOfParts);
@@ -455,10 +556,10 @@
     tmp$ = spawn_0.room.find(FIND_SOURCES);
     loop: for (var i = 0; i !== tmp$.length; ++i) {
       var tmp$_0;
-      if (shouldBuildMoreWorkers(get_buildersNeeded(roomMemory).get_za3lpa$(i), creeps, Role$BUILDER_getInstance(), i))
-        tmp$_0 = Role$BUILDER_getInstance();
-      else if (shouldBuildMoreWorkers(get_harvestersNeeded(roomMemory).get_za3lpa$(i), creeps, Role$HARVESTER_getInstance(), i))
+      if (shouldBuildMoreWorkers(get_harvestersNeeded(roomMemory).get_za3lpa$(i), creeps, Role$HARVESTER_getInstance(), i))
         tmp$_0 = Role$HARVESTER_getInstance();
+      else if (shouldBuildMoreWorkers(get_buildersNeeded(roomMemory).get_za3lpa$(i), creeps, Role$BUILDER_getInstance(), i))
+        tmp$_0 = Role$BUILDER_getInstance();
       else if (shouldBuildMoreWorkers(get_upgradersNeeded(roomMemory).get_za3lpa$(i), creeps, Role$UPGRADER_getInstance(), i))
         tmp$_0 = Role$UPGRADER_getInstance();
       else if (shouldBuildMoreWorkers(get_repairersNeeded(roomMemory).get_za3lpa$(i), creeps, Role$REPAIRER_getInstance(), i))
@@ -483,7 +584,7 @@
     };
   }
   function spawn(spawn, body, role, resource) {
-    var newName = toString(Kotlin.getKClassFromExpression(role).simpleName) + '_' + Game.time;
+    var newName = role.toString() + '_' + Game.time;
     var code = spawn.spawnCreep(body, newName, options(spawn$lambda(role, resource)));
     if (equals(code, OK))
       console.log('spawning ' + newName + ' with body ' + body);
@@ -503,15 +604,22 @@
     }));
   }
   function complexMemory$lambda_0(it) {
+    var tmp$, tmp$_0;
+    var tmp$_1;
     var $receiver = split(it, [',']);
-    var destination = ArrayList_init(collectionSizeOrDefault($receiver, 10));
-    var tmp$;
-    tmp$ = $receiver.iterator();
-    while (tmp$.hasNext()) {
-      var item = tmp$.next();
-      destination.add_11rb$(toInt(item));
-    }
-    return destination;
+    var tmp$_2, tmp$_3;
+    if ((tmp$ = ((tmp$_3 = (tmp$_2 = firstOrNull_0($receiver)) != null ? tmp$_2.length > 0 : null) != null ? tmp$_3 : false) ? $receiver : null) != null) {
+      var destination = ArrayList_init(collectionSizeOrDefault(tmp$, 10));
+      var tmp$_4;
+      tmp$_4 = tmp$.iterator();
+      while (tmp$_4.hasNext()) {
+        var item = tmp$_4.next();
+        destination.add_11rb$(toInt(item));
+      }
+      tmp$_1 = destination;
+    } else
+      tmp$_1 = null;
+    return (tmp$_0 = tmp$_1) != null ? tmp$_0 : emptyList();
   }
   function complexMemory(defaultValue) {
     return new MemoryMappingDelegate(defaultValue, complexMemory$lambda, complexMemory$lambda_0);
@@ -574,6 +682,17 @@
   }
   function set_resourceIndex($receiver, resourceIndex_0) {
     resourceIndex.setValue_9rddgb$($receiver, resourceIndex_metadata, resourceIndex_0);
+  }
+  function targetId$lambda() {
+    return '';
+  }
+  var targetId;
+  var targetId_metadata = new PropertyMetadata('targetId');
+  function get_targetId($receiver) {
+    return targetId.getValue_lrcp0p$($receiver, targetId_metadata);
+  }
+  function set_targetId($receiver, targetId_0) {
+    targetId.setValue_9rddgb$($receiver, targetId_metadata, targetId_0);
   }
   var temporaryTask;
   var temporaryTask_metadata = new PropertyMetadata('temporaryTask');
@@ -653,7 +772,7 @@
     var tmp$, tmp$_0, tmp$_1;
     console.log('look starting');
     var $receiver = Game.spawns;
-    tmp$ = firstOrNull_0(Object.values($receiver));
+    tmp$ = firstOrNull_1(Object.values($receiver));
     if (tmp$ == null) {
       return console.log('No spawn found, exiting...');
     }var mainSpawn = tmp$;
@@ -728,19 +847,31 @@
   }
   _.loop = loop;
   var package$building = _.building || (_.building = {});
+  package$building.get_priority_wvximt$ = get_priority;
+  Object.defineProperty(StructurePriority, 'LOW', {
+    get: StructurePriority$LOW_getInstance
+  });
+  Object.defineProperty(StructurePriority, 'MEDIUM', {
+    get: StructurePriority$MEDIUM_getInstance
+  });
+  Object.defineProperty(StructurePriority, 'HIGH', {
+    get: StructurePriority$HIGH_getInstance
+  });
+  package$building.StructurePriority = StructurePriority;
   package$building.buildExtensionsCloseToController_r9ibs2$ = buildExtensionsCloseToController;
-  package$building.buildRoadsToSources = buildRoadsToSources;
   package$building.buildRoad_g9xrns$ = buildRoad;
   package$building.ensureCriticalRoadsExists_x8x31k$ = ensureCriticalRoadsExists;
   package$building.buildRoadBetweenSourcesAndController_x8x31k$ = buildRoadBetweenSourcesAndController;
   package$building.buildRoadBetweenSpawnAndSources_x8x31k$ = buildRoadBetweenSpawnAndSources;
   $$importsForInline$$['screeps-kotlin-types-jsLegacy'] = $module$screeps_kotlin_types_jsLegacy;
   var package$creeps = _.creeps || (_.creeps = {});
-  package$creeps.build_hrmv4t$ = build;
+  package$creeps.build_z15gke$ = build;
   package$creeps.buildingIsPossible_hrmv4t$ = buildingIsPossible;
   package$creeps.harvestIsPossible_kaa3hh$ = harvestIsPossible;
   package$creeps.closestEnergyStoreWithSpace_hrmv4t$ = closestEnergyStoreWithSpace;
   package$creeps.harvest_g1gq2n$ = harvest;
+  package$creeps.goHarvest_qt6aql$ = goHarvest;
+  package$creeps.walkTo_5vdfsc$ = walkTo;
   package$creeps.repair_5rb157$ = repair;
   package$creeps.repair_3gl8b2$ = repair_0;
   package$creeps.repairIsPossible_hrmv4t$ = repairIsPossible;
@@ -784,6 +915,8 @@
   package$memory.set_role_2apcn8$ = set_role;
   package$memory.get_resourceIndex_wwybnu$ = get_resourceIndex;
   package$memory.set_resourceIndex_w6vdec$ = set_resourceIndex;
+  package$memory.get_targetId_wwybnu$ = get_targetId;
+  package$memory.set_targetId_57vlho$ = set_targetId;
   package$memory.get_temporaryTask_wwybnu$ = get_temporaryTask;
   package$memory.set_temporaryTask_2apcn8$ = set_temporaryTask;
   package$memory.get_numberOfCreeps_4xuaja$ = get_numberOfCreeps;
@@ -810,6 +943,7 @@
     return $receiver.name;
   }), memory$lambda_0(Role, Kotlin.isInstanceOf(Role)));
   resourceIndex = memory(resourceIndex$lambda);
+  targetId = memory(targetId$lambda);
   temporaryTask = new MemoryMappingDelegate(memory$lambda(Role$UNASSIGNED_getInstance()), getPropertyCallableRef('name', 1, function ($receiver) {
     return $receiver.name;
   }), memory$lambda_0(Role, Kotlin.isInstanceOf(Role)));
